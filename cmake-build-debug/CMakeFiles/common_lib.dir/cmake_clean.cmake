300cf425fe07fa1705f54b15f608e5c36b2d9991
Zhenli WU <zhenliwu@Zhenlis-MacBook-Pro.local>
2018-03-04 23:52:09 -0500

    tool for labeling

---
+++ cmake-build-debug/CMakeFiles/common_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/common_lib.dir/src/floatimage.cpp.o"
+  "libcommon_lib.pdb"
+  "libcommon_lib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/common_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/yangtze.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yangtze.dir/src/main.cpp.o"
+  "yangtze.pdb"
+  "yangtze"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yangtze.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
